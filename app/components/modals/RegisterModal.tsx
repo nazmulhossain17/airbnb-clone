3c50bafa20176a5ea74bfdf0a4bb20ce51bead3f
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2023-08-22 14:10:14 +0600

    package.json

---
+++ app/components/modals/RegisterModal.tsx
@@ -0,0 +1,6 @@
+"use client";
+const RegisterModal = () => {
+  return <div></div>;
+};
+
+export default RegisterModal;
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Nunito } from "next/font/google";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
 import Modal from "./components/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
 
 const font = Nunito({
   subsets: ["latin"],
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <RegisterModal />
           <Modal actionLabel="Submit" title="Welcome" isOpen />
           <Navbar />
         </ClientOnly>
