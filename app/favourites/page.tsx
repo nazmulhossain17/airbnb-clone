5f7feaa10b6902236807e3a2a5566589d2a4d372
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2023-10-26 18:52:13 +0600

    favourite client

--- app/favourites/page.tsx
+++ app/favourites/page.tsx
@@ -1,13 +1,27 @@
+import getCurrentUser from "../actions/getCurrentUser";
+import getFavoriteListings from "../actions/getFavouriteListings";
 import ClientOnly from "../components/ClientOnly";
 import EmptyState from "../components/EmptyState";
+import FavoritesClient from "./FavoritesClient";
 
 const ListingPage = async () => {
+  const listings = await getFavoriteListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="No favorites found"
+          subtitle="Looks like you have no favorite listings."
+        />
+      </ClientOnly>
+    );
+  }
+
   return (
     <ClientOnly>
-      <EmptyState
-        title="No favourite food found"
-        subtitle="Looks like you have no favourite listings"
-      />
+      <FavoritesClient listings={listings} currentUser={currentUser} />
     </ClientOnly>
   );
 };
