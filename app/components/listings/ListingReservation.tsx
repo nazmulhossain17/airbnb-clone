0816c1d3381f4f2e8f1b4b9f91069d9f1e88a99e
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2023-10-26 06:43:27 +0600

    Calender

--- app/components/listings/ListingReservation.tsx
+++ app/components/listings/ListingReservation.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Range } from "react-date-range";
+import Calender from "../inputs/Calender";
 
 interface ListingReservationProps {
   price: number;
@@ -25,7 +26,14 @@ const ListingReservation: React.FC<ListingReservationProps> = ({
     <div className="bg-white rounded-xl border-[1px] border-neutral-200 overflow-hidden">
       <div className="flex flex-row items-center gap-1 p-4">
         <div className="text-2xl font-semibold">${price}</div>
+        <div className="font-light text-neutral-600">night</div>
       </div>
+      <hr />
+      <Calender
+        value={dateRange}
+        disabledDates={disabledDates}
+        onChange={(value) => onChangeDate(value.selection)}
+      />
     </div>
   );
 };
