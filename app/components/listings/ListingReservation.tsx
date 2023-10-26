0465eedc885621831fbdba80aaea8555c70ff351
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2023-10-26 22:19:13 +0600

    *

--- app/components/listings/ListingReservation.tsx
+++ app/components/listings/ListingReservation.tsx
@@ -10,7 +10,7 @@ interface ListingReservationProps {
   totalPrice: number;
   onChangeDate: (value: Range) => void;
   onSubmit: () => void;
-  disabled: boolean;
+  disabled?: boolean;
   disabledDates: Date[];
 }
 
@@ -24,9 +24,19 @@ const ListingReservation: React.FC<ListingReservationProps> = ({
   disabledDates,
 }) => {
   return (
-    <div className="bg-white rounded-xl border-[1px] border-neutral-200 overflow-hidden">
-      <div className="flex flex-row items-center gap-1 p-4">
-        <div className="text-2xl font-semibold">${price}</div>
+    <div
+      className="
+      bg-white 
+        rounded-xl 
+        border-[1px]
+      border-neutral-200 
+        overflow-hidden"
+    >
+      <div
+        className="
+      flex flex-row items-center gap-1 p-4"
+      >
+        <div className="text-2xl font-semibold">$ {price}</div>
         <div className="font-light text-neutral-600">night</div>
       </div>
       <hr />
@@ -48,8 +58,7 @@ const ListingReservation: React.FC<ListingReservationProps> = ({
           items-center 
           justify-between
           font-semibold
-          text-lg
-        "
+          text-lg"
       >
         <div>Total</div>
         <div>$ {totalPrice}</div>
