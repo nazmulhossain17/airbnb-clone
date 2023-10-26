970ea7b6b9a8d758bb62ec87e65a5c142a0a5f30
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2023-10-26 22:10:19 +0600

    ^

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,10 +1,8 @@
 import getCurrentUser from "../actions/getCurrentUser";
 import getListings from "../actions/getListings";
-import getReservations from "../actions/getReservations";
 import ClientOnly from "../components/ClientOnly";
 import EmptyState from "../components/EmptyState";
 import PropertiesClient from "./PropertiesClient";
-import TripsClient from "./TripsClient";
 
 const PropertiesPage = async () => {
   const currentUser = await getCurrentUser();
