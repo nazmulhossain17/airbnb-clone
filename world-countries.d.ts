89fe0c73a7df7db8839c9c22f106418efbe7d7e1
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2024-12-29 12:09:06 +0600

    update module

---
+++ world-countries.d.ts
@@ -0,0 +1,13 @@
+declare module "world-countries" {
+    interface Country {
+      cca2: string;
+      name: { common: string; official: string };
+      flag: string;
+      latlng: [number, number];
+      region: string;
+    }
+  
+    const countries: Country[];
+    export default countries;
+  }
+  
